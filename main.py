00ec5ceb3383b39232fe82e97809cb2cee435d31
lucas serra <lucasserradevelopment@gmail.com>
2025-08-23 22:07:23 -0300

    initial project structure

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import discord
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+token = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'bot has started was {client.user}')
+
+client.run(token)
